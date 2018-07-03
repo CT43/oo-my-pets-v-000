3605580db73133a27f46a2d990628d91ee396d86
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-03 17:46:43 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -37,4 +37,14 @@ class Owner
     @pets[:fishes] << new_fish
   end
 
+  def buy_cat(name)
+    new_cat = Cat.new(name)
+    @pets[:cats] << new_cat 
+  end 
+
+  def buy_dog (name)
+    new_dog = Dog.new(name)
+    @pets[:dogs] << new_cat 
+  end 
+
 end
